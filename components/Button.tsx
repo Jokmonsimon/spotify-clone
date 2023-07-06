b6e16132fafe07e56270878a3f1c4d6767004fb1
Ojok Simon Peter <ojok@yearnaiafrica.com>
2023-07-06 20:03:34 +0300

    Add sign up button

--- components/Button.tsx
+++ components/Button.tsx
@@ -9,9 +9,12 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
       <button
         type={type}
         className={twMerge(
-          'w-full rounded-md bg-blue-500 border border-transparent px-3 py-3 disabled:cursor-not-allowed disabled:opacity-75 text-black font-bold hover:opacity-75 transition',
+          'w-full rounded-md bg-blue-500 border border-transparent px-2 py-2 disabled:cursor-not-allowed disabled:opacity-75 text-black font-bold hover:opacity-75 transition',
           className
         )}
+        disabled={disabled}
+        ref={ref}
+        {...props}
       >
         Sign Up
       </button>
